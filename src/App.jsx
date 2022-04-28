09431e86b9a21bb509dc86ebb6170b0441c53217
up1 <somkiat.p@gmail.com>
2022-04-28 11:51:39 +0700

    Demo with class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import './App.css'
+
+class AppClass extends Component {
+  message = "Demo";
+
+  constructor() {
+    super();
+    this.state = {
+      count: 10,
+    };
+  }
+
+  setCount(newValue) {
+    this.setState( { count:  newValue} );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.message}</p>
+          <p>
+            <button type="button" 
+              onClick={() => this.setCount(this.state.count + 1)}>
+              count is: {this.state.count}
+            </button>
+          </p>
+        </header>
+      </div>
+    )
+  };
+}
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -20,4 +51,4 @@ const App = () => {
   )
 }
 
-export default App
+export default AppClass
