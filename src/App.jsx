c129e1349f2d303f5e89f57e65bcc4cf02265afd
up1 <somkiat.p@gmail.com>
2022-04-28 11:55:30 +0700

    Separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
 import { Component, useState } from 'react'
 import './App.css'
 
-class AppClass extends Component {
-  message = "Demo";
-
-  constructor() {
-    super();
-    this.state = {
-      count: 10,
-    };
-  }
-
-  setCount(newValue) {
-    this.setState( { count:  newValue} );
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>{this.message}</p>
-          <p>
-            <button type="button" 
-              onClick={() => this.setCount(this.state.count + 1)}>
-              count is: {this.state.count}
-            </button>
-          </p>
-        </header>
-      </div>
-    )
-  };
-}
-
 const App = () => {
   const [count, setCount] = useState(0)
-  const message = "Hello React from variable!";
+  const message = "Hello Function Component!";
 
   return (
     <div className="App">
@@ -51,4 +20,4 @@ const App = () => {
   )
 }
 
-export default AppClass
+export default App
