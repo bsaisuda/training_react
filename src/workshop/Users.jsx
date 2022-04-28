fab50e8cf8f411be487571a6797ab7a97c7567a5
up1 <somkiat.p@gmail.com>
2022-04-28 16:24:07 +0700

    useEffect

--- src/workshop/Users.jsx
+++ src/workshop/Users.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
 
   // Call data from https://jsonplaceholder.typicode.com/users
-  axios.get('https://jsonplaceholder.typicode.com/users')
-  .then(function (response) {
-    console.log(response);
-	// setUsers(response.data);
-  })
+  useEffect( () => {
+	console.log("Use effect");
+	axios.get('https://jsonplaceholder.typicode.com/users')
+	.then(function (response) {
+		setUsers(response.data);
+	})
+
+  }, []);
+  
+  console.log(users);
 
   return (
     <div>
 		  Demo with API.
+		  {users.map(user => <li key={user.id}>{user.name}</li>)}
     </div>
   )
 }
