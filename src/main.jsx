d560477091fc9b314eb5324ecdce1f485d113fad
up1 <somkiat.p@gmail.com>
2022-04-28 14:44:18 +0700

    Workshop 01

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Workshop from './workshop/Workshop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <Workshop/>
   </React.StrictMode>
 )
---
+++ src/workshop/Hello.jsx
@@ -0,0 +1,12 @@
+const Hello = () => {
+  return (
+    <div>
+      <form>
+		Name : <input type="text" />
+	  </form>
+	  Please enter your name
+    </div>
+  )
+}
+
+export default Hello
